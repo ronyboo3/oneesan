a36f4e6b0c06444d53e6926b5c840435df4f8993
tanase <ronyboo333@gmail.com>
2016-10-10 01:23:12 +0900

    とりあえず公開

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jquery-turbolinks'
 
 gem 'nokogiri'
 
+gem 'twitter'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ lib/cron/tweet.rb
@@ -0,0 +1,23 @@
+require 'cron/make_data_from_rss'
+config = {
+  consumer_key: 'OOLv7qbP9mFJgOLa05xz3EmzS',
+  consumer_secret: '6xKOzK9h9Ny2hyK2JFre4uqOxIuDwXLwYRNTlbX097emq15rjF',
+  access_token: '775221815887081472-ayCnuS5vikVdpwxB7lt2y38zBnyi3KZ',
+  access_token_secret: 'x0Cr2nA5eumVF3U34ubti5Y9MDRC6rXQIzSfuQwqkItn8',
+}
+
+message = "ねぇねぇ、さっき聞いた話なんだけど・・・\n\n"
+
+make_data_from_rss = MakeDataFromRss.new("omoshiro")
+articles = make_data_from_rss.articles
+
+message += "「#{articles[0]["title"]}」\n\n"
+message += "これ今話題の記事なんだって！\n\n"
+
+message += "詳しくはこちら\n"
+message += "http://bot-jp.com\n\n"
+message += "#噂のお姉さん"
+
+client = Twitter::REST::Client.new(config)
+
+client.update(message)
